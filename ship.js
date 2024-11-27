0e67810db97c8dbf579c3f5700e4aa98276486be
adriansito <adriancrosby02@gmail.com>
2024-11-26 23:02:00 -0800

    created Ship object and corresponding tests

---
+++ ship.js
@@ -0,0 +1,16 @@
+module.exports = class Ship {
+	constructor(len, hits, sunk) {
+		(this.len = len), (this.hits = hits), (this.sunk = sunk);
+	}
+	hit() {
+		this.hits += 1;
+		this.sunk = this.isSunk();
+		return this.hits;
+	}
+	isSunk() {
+		if (this.hits >= this.len) {
+			return (this.sunk = true);
+		}
+		return false;
+	}
+};
